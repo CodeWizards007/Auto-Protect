04a84880ced32f7b695a5a74f1b8be1944d2ba67
saifeddine-houji <saifeddine.houji@esprit.tn>
2022-10-23 16:40:56 +0100

    sidebar fix

--- src/app/back/back-routing.module.ts
+++ src/app/back/back-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { BackComponent } from './back.component';
 import {LoginComponent} from "../login/login.component";
 import {ExpertAddComponent} from "./expert/expert-add/expert-add.component";
+import {CreateResponsableComponent} from "./create-responsable/create-responsable.component";
+import {CreateAssureurComponent} from "./create-assureur/create-assureur.component";
+import {AdminStatsComponent} from "./admin-stats/admin-stats.component";
 
 const routes: Routes = [{
   path: '', component: BackComponent,
   children: [
+    {path: 'ajoutResponsable', component: CreateResponsableComponent},
     {path: 'ajoutexpert', component: ExpertAddComponent},
-
+    {path: 'ajoutAssureur', component: CreateAssureurComponent},
+    {path: 'adminStats', component: AdminStatsComponent},
 
   ]
 },
---
+++ src/app/back/create-responsable/create-responsable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-responsable',
+  templateUrl: './create-responsable.component.html',
+  styleUrls: ['./create-responsable.component.css']
+})
+export class CreateResponsableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
